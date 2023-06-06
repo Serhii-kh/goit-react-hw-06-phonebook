eac0e46c719a3e713c55e218d5c69c9ec55ddc76
Serhii Naboka <drifterzzz@gmail.com>
2023-06-06 21:39:14 +0300

    jhj

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
-import { store } from './components/store/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<Provider store={store}>
+	<Provider>
   <React.StrictMode>
       <App />
 		</React.StrictMode>
