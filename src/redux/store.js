ccc4980fe95c5844e5b6a91ab41ca6e026fbe7c5
Serhii Naboka <drifterzzz@gmail.com>
2023-06-10 10:47:10 +0300

    upd

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { ContactsListSlice } from './ContactsListSlice';
 import { FilterSlice } from './FilterSlice';
 
-// const initialState = {
-//   contacts: [],
-//   filter: '',
-// };
-
 export const store = configureStore({
-  // initialState,
-
   reducer: {
     contacts: ContactsListSlice.reducer,
     filter: FilterSlice.reducer,
